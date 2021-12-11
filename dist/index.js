70a9539966b8ccd61ce0a87b71f0553dc97f2a01
Shimpei Wakida <[shimpei.wakida@gmail.com>
2021-12-12 01:50:13 +0900

    最初の動作確認

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,4 @@
 var sayHello = function (name) {
     return "Hello, " + name;
 };
-console.log(sayHello('Michael Jacksoon'));
+process.stdout.write(sayHello('Michael Jackson'));
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ const sayHello = (name: string) => {
   return `Hello, ${name}`
 }
 
-console.log(sayHello('Michael Jacksoon'))
+process.stdout.write(sayHello('Michael Jackson'))
